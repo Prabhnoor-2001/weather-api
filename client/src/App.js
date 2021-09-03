6267d21debb2eb731f2afc7d92677105c0dd6c1e
LouisDVL <louis.lising@outlook.com>
2021-09-03 15:09:08 +1200

    completed post route

--- client/src/App.js
+++ client/src/App.js
@@ -1,81 +1,82 @@
-import React, { useEffect, useState } from 'react'
-import { Route } from 'react-router-dom'
-import { getTemp } from './api'
-import SearchBar from './components/searchBar'
-import Review from './components/Review'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import { Route } from "react-router-dom";
+import { getTemp } from "./api";
+import SearchBar from "./components/searchBar";
+import Review from "./components/Review";
+import "./App.css";
+import WriteReview from "./components/WriteReview";
 
-function App () {
-  const [currTemp, setTemp] = useState({})
-  const [isLoading, setLoad] = useState(true)
-  const [weather, setWeather]= useState({})
-  const [currentCity, setCity] =  useState('')
-  useEffect(()=>{
-    getTemperature()
-  },[])
+function App() {
+  const [currTemp, setTemp] = useState({});
+  const [isLoading, setLoad] = useState(true);
+  const [weather, setWeather] = useState({});
+  const [currentCity, setCity] = useState("");
+  useEffect(() => {
+    getTemperature();
+  }, []);
 
-  function getTemperature(){
-    if(isLoading){
-      getTemp("copenhagen")
-      .then((data)=>{
-        setTemp(data.main)
-        setWeather(data.weather[0])
-        setLoad(false)
-        return null
-      })
-      .catch((err)=>{
-        console.log("hey error");
-      })
+  function getTemperature() {
+    if (isLoading) {
+      getTemp("auckland")
+        .then((data) => {
+          setTemp(data.main);
+          setWeather(data.weather[0]);
+          setLoad(false);
+          return null;
+        })
+        .catch((err) => {
+          console.log("hey error");
+        });
+    } else {
+      getTemp(currentCity)
+        .then((data) => {
+          setTemp(data.main);
+          setWeather(data.weather[0]);
+          setLoad(false);
+          return null;
+        })
+        .catch((err) => {
+          console.log("hey error");
+        });
     }
-   else{ getTemp(currentCity)
-      .then((data)=>{
-        setTemp(data.main)
-        setWeather(data.weather[0])
-        setLoad(false)
-        return null
-      })
-      .catch((err)=>{
-        console.log("hey error");
-      })}
   }
-  function handleChange (event) {
+  function handleChange(event) {
     //setSearch(event.target.value)
     //console.log(event.target.value);
-    handleCityName(event.target.value)
+    handleCityName(event.target.value);
   }
-  function handleCityName (name) {
-    setCity(name)
+  function handleCityName(name) {
+    setCity(name);
     //console.log(currentCity);
   }
   //console.log(currTemp);
   return (
     <>
-    <Route exact path='/'>
+      <Route exact path="/">
+        <div className="search">
+          {isLoading ? (
+            "hi"
+          ) : (
+            <SearchBar
+              placeholder="City name..."
+              handleChange={(event) => {
+                handleChange(event);
+              }}
+              getTemperature={getTemperature}
+            />
+          )}
+        </div>
 
-    <div className="search">
-      {
-        isLoading ? 'hi':
-        <SearchBar
-            placeholder='City name...'
-            handleChange={(event) => {
-              handleChange(event)
-            }}
-            getTemperature={getTemperature}
-          />
-      }
-    </div>
-
-    <div className="data">
-      <h1>{isLoading ? 'hi':currTemp.temp}°C</h1>
-      <h1>{isLoading ? 'hi':weather.main}</h1>
-      <h2>{isLoading ? 'hi':weather.description}</h2>
-    </div>
-
-    </Route>
-    <Route path='/reviews' component={Review}>
-    </Route>
+        <div className="data">
+          <h1>{isLoading ? "hi" : currTemp.temp}°C</h1>
+          <h1>{isLoading ? "hi" : weather.main}</h1>
+          <h2>{isLoading ? "hi" : weather.description}</h2>
+        </div>
+      </Route>
+      <Route path="/new/review" component={WriteReview} />
+      <Route path="/reviews" component={Review} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/WriteReview.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import { postNewComment } from "../api";
+
+function WriteReview() {
+  const [name, setName] = useState("");
+  const [review, setReview] = useState("");
+
+  function onSubmit() {
+    const newReview = {
+      name,
+      comments: review,
+    };
+    postNewComment(newReview);
+  }
+  return (
+    <div className="formContainer">
+      <div className="form">
+        <h1>Give us A Review</h1>
+        <form
+          className="formText"
+          onSubmit={(e) => {
+            e.preventDefault();
+            onSubmit();
+          }}
+        >
+          <label>Name: </label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            value={name}
+          />
+          <label>Review: </label>
+          <textarea
+            type="text"
+            onChange={(e) => {
+              setReview(e.target.value);
+            }}
+            value={review}
+            rows="4"
+            cols="50"
+          >
+            {review}
+          </textarea>
+          <button className="submitButton">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default WriteReview;
